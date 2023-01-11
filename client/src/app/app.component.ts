e1f46e5a6bea36b1ed8413b172d6fa9361f5ec67
Godwin Onah <godwinonah@godwins-air.home>
2023-01-11 17:49:33 +0000

    Solved Cors policy erros problem by adding services.AddCors(<Policy name and Expression>)and app.AddCors(policy name) to the pipeline methods

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  
+  title = 'BagShop';
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7135/products/2').subscribe((response:any)=>{
+      console.log(response);
+
+    },error=>{console.log(error)}
+  )};
 }
