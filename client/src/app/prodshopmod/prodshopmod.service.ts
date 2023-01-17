0016872d740aac6ada330bbb029e08a102ee0cfc
Godwin Onah <godwinonah@godwins-air.home>
2023-01-17 10:11:48 +0000

    Added product details to a different componemt when selected

--- client/src/app/prodshopmod/prodshopmod.service.ts
+++ client/src/app/prodshopmod/prodshopmod.service.ts
@@ -4,6 +4,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { IBrands } from '../prodsharemod/models/IBrands';
 import { IProductTypes } from '../prodsharemod/models/IProductTypes';
 import { map } from 'rxjs';
+import { ShopParameters } from '../prodsharemod/models/shopParameters';
+import { IProduct } from '../prodsharemod/models/IProduct';
 @Injectable({
   providedIn: 'root'
 })
@@ -15,30 +17,42 @@ export class ProdshopmodService {
   constructor(private http: HttpClient) {
    }
 
-   getProducts(brandId?:number,typeId?:number,sort?:string){
+   getProducts(shopParameters:ShopParameters){
 
     let params=new HttpParams();
 
-    if(brandId){
-      params=params.append('brandId',brandId.toString())
+    if(shopParameters.brandId!=0){
+      params=params.append('brandId',shopParameters.brandId.toString())
 
     }
 
-    if(typeId){
-      params=params.append('typeId',typeId.toString())
+    if(shopParameters.typeId!=0){
+      params=params.append('typeId',shopParameters.typeId.toString())
+ 
+    }
+
+
+    if(shopParameters.search){
+      params=params.append('search',shopParameters.search)
 
     }
 
-    if(sort){
-
-      params=params.append('sort',sort)
-    }
+      params=params.append('sort',shopParameters.sort)
+      params=params.append('pageIndex',shopParameters.pageNumber.toString())
+      params=params.append('pageIndex',shopParameters.pageSize.toString())
+      
+    
 
     return this.http.get<IProductPagination>
     (this.url+'products',{observe:'response',params})
     .pipe(map(
       response=> response.body)
     )
+   }
+
+   getProduct(productId:number){
+
+    return this.http.get<IProduct>(this.url+"products/"+productId)
    }
 
    getBrands(){
