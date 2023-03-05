3dd54885e67a0f8a72575320b5557bbd97545eb0
Godwin Onah <godwinonah@godwins-air.home>
2023-03-05 16:39:22 +0000

    Added client-side payment card validation and enabled linear mode for stepper component

--- client/src/app/prodsharemod/components/stepper/stepper.component.ts
+++ client/src/app/prodsharemod/components/stepper/stepper.component.ts
@@ -1,5 +1,5 @@
 import { CdkStepper } from '@angular/cdk/stepper';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stepper',
@@ -7,8 +7,15 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./stepper.component.scss'],
   providers:[{provide:CdkStepper, useExisting:StepperComponent}]
 })
-export class StepperComponent extends CdkStepper{
+export class StepperComponent extends CdkStepper implements OnInit{
+
   @Input() linearModeSelected = true;
+
+  
+  ngOnInit(): void {
+    this.linear = this.linearModeSelected;
+  }
+ 
 
   OnClick(index:number){
       this.selectedIndex = index;
